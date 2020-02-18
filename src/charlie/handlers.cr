5276cef0f5342393e488dc35bb762c44c200cdc2
Crystal Danger <cdneal091@gmail.com>
2020-02-18 16:31:29 -0500

    recieves board data as json

--- src/charlie/handlers.cr
+++ src/charlie/handlers.cr
@@ -1,5 +1,14 @@
 class Charlie
-  def handle_message(message)
-
+  def self.handle_message(message : String, socket : HTTP::WebSocket)
+    if (message == "base")
+      {"boardList": board_list,
+        "announce": "This site is still under development."
+      }
+    else
+      return {"error" => "Bad Request"} unless CC.board_list.keys.includes?(message)
+      {"boardDump" => {
+        "#{message}" => board_by_id(message)
+        }}
+    end
   end
 end
