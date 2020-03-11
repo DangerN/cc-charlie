9960b42b81ffa1cf5cca25128574d3913a082621
Crystal Danger <cdneal091@gmail.com>
2020-03-11 16:39:19 -0400

    forgot what I changed lol

--- src/charlie/delta_connection.cr
+++ src/charlie/delta_connection.cr
@@ -3,7 +3,6 @@ class Charlie
     socket = HTTP::WebSocket.new("0.0.0.0", "/", port: 9002)
 
     socket.on_message do |message|
-
       Alpha.boards = JSON.parse(message).as_h.transform_values { |board| Alpha::Board.from_json(board.to_json) }
       @@subscribers.each do |subscriber|
         message = {"type"=> "fullImage", "image"=> Alpha.boards}.to_json
