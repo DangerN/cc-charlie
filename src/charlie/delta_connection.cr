430ef87fe02b68c4e71d22c5a34597b2b77301a2
Crystal Danger <cdneal091@gmail.com>
2020-03-28 14:40:06 -0400

    auto reconnects to delta

--- src/charlie/delta_connection.cr
+++ src/charlie/delta_connection.cr
@@ -1,15 +1,24 @@
 class Charlie
   def self.delta_connection
+    p "Atteming to connect to delta"
     socket = HTTP::WebSocket.new("0.0.0.0", "/", port: 9002)
-
+  rescue Errno
+    sleep 2
+    delta_connection
+  else
     socket.on_message do |message|
       Alpha.boards = JSON.parse(message).as_h.transform_values { |board| Alpha::Board.from_json(board.to_json) }
       @@subscribers.each do |subscriber|
-        message = {"type"=> "fullImage", "image"=> Alpha.boards}.to_json
+        message = {"type" => "fullImage", "image" => Alpha.boards}.to_json
         subscriber.send message
       end
     end
 
+    socket.on_close do
+      p "Lost Delta"
+      sleep 2
+      delta_connection
+    end
     spawn do
       socket.run
     end
--- src/charlie/handlers.cr
+++ src/charlie/handlers.cr
@@ -2,15 +2,15 @@ class Charlie
   def self.handle_message(message : String, socket : HTTP::WebSocket)
     if (message == "base")
       {"boardList": board_list,
-        "announce": "This site is still under development.",
-        "type": "boardList"
+       "announce":  "This site is still under development.",
+       "type":      "boardList",
       }
     else
       return {"error" => "Bad Request"} unless board_list.keys.includes?(message)
-      {"type" => "boardDump",
-        "boardDump"=> {
-        "#{message}"=> board_by_id(message)
-        }}
+      {"type"      => "boardDump",
+       "boardDump" => {
+         "#{message}" => board_by_id(message),
+       }}
     end
   end
 end
